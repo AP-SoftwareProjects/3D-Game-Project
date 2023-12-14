180c17ad1d5a5ea9c1bc3f1de2b23a352f3280f8
BotzzMonkey31 <145664630+BotzzMonkey31@users.noreply.github.com>
2023-12-14 15:52:06 +0100

    PickUpAnimation
    Script for pick up and pick animation

---
+++ Game Project/Assets/script/Dissapear.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dissapear : MonoBehaviour
+{
+
+    public GameObject MainCanvas;
+    Text points;
+    // Start is called before the first frame update
+    void Start()
+    {
+        points = MainCanvas.GetComponent<Text>();   
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "player")
+        {
+            Destroy(gameObject);
+            points.text = (int.Parse(points.text) + 1).ToString();
+        }
+        
+    }
+
+}
