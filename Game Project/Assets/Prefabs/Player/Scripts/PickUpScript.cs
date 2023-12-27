bcbb06876ad4944591053c8f60f4335ad114a015
Timothy-N <timothynuyts92@gmail.com>
2023-12-27 14:43:58 +0100

    added timer on screen

--- Game Project/Assets/Prefabs/Player/Scripts/PickUpScript.cs
+++ Game Project/Assets/Prefabs/Player/Scripts/PickUpScript.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.Burst.CompilerServices;
+using Unity.VisualScripting.Dependencies.NCalc;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PickUpScript : MonoBehaviour
 {
@@ -17,10 +20,24 @@ public class PickUpScript : MonoBehaviour
     private float _cooldownTimer = 0f;
 
     private GameObject _trashObject;
+    public GameObject focusObject;
+    public Canvas tagObject;
+
+
+    
+    [SerializeField] private Image uiFill;
+    [SerializeField] private TextMeshProUGUI uiText;
+
+    //public int Duration;
+
+    private int remainingDuration;
+    
 
     void Start()
     {
         _animator = GetComponent<Animator>();
+
+        tagObject.gameObject.SetActive(false);
     }
 
     void Update()
@@ -32,12 +49,25 @@ public class PickUpScript : MonoBehaviour
         {
             if (IsCloseToTrash())
             {
+                tagObject.gameObject.SetActive(true);
+                Being(Convert.ToInt32(pickupDuration)); //
                 PlayerManager.Instance.IsPicking = true;
                 _animator.SetBool("PickingUp", true);
 
                 _cooldownTimer = pickupDuration;
                 _pickupTimer = pickupDuration;
+
+
+
+                
+                //   Vector3 directionToTarget = focusObject.transform.position - transform.position;
+                // Quaternion rotationToTarget = Quaternion.LookRotation(directionToTarget, Vector3.up);
+
+                //    rotationToTarget *= Quaternion.Euler(0f, 180f, 0f);
+                //  tagObject.transform.rotation = rotationToTarget;
+
             }
+
         }
 
 
@@ -84,5 +114,32 @@ public class PickUpScript : MonoBehaviour
     {
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(transform.position, pickupRange);
+    }
+    
+
+    //timer code
+    private void Being(int Second)
+    {
+        remainingDuration = Second;
+        StartCoroutine(UpdateTimer());
+    }
+    private IEnumerator UpdateTimer()
+    {
+        while(remainingDuration >= 0)
+        {
+        uiText.text = $"{remainingDuration / 60:00}:{remainingDuration % 60:00}";
+        uiFill.fillAmount = Mathf.InverseLerp(0, pickupDuration, remainingDuration); //
+        remainingDuration--;
+        yield return new WaitForSeconds(1f);
+        }
+        if(remainingDuration < 0)
+        {
+            tagObject.gameObject.SetActive(false);
+        }
+
+    }
+    private void onEnd()
+    {
+        print("end");
     }
 }
