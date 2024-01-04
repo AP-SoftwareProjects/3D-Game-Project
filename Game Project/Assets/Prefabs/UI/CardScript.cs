1cbaf0bda1a36a9aa54e8317f2c6adeec0341f4d
eyupc <eyuptrabzon@icloud.com>
2024-01-05 00:22:15 +0100

    feat: adds script for cards to draw the icons & texts

---
+++ Game Project/Assets/Prefabs/UI/CardScript.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardScript : MonoBehaviour
+{
+
+    public string title;
+    public Sprite iconImg;
+    public float priceValue;
+
+
+    private void Start()
+    {
+        this.transform.Find("TRASH_SPAWN_TEXT").GetComponent<Text>().text = title;
+        this.transform.Find("TRASH_SPAWN_ICON").GetComponent<Image>().sprite = iconImg;
+        this.transform.Find("BUY_BUTTON").Find("BUY_BUTTON_TEXT").GetComponent<Text>().text = priceValue.ToString();
+
+    }
+}
