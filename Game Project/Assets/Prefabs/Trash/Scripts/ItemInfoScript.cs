24086700ed00b2611d0a8dfc7b2953b73bd646c6
Timothy-N <timothynuyts92@gmail.com>
2024-01-23 15:49:55 +0100

    added the bintype

--- Game Project/Assets/Prefabs/Trash/Scripts/ItemInfoScript.cs
+++ Game Project/Assets/Prefabs/Trash/Scripts/ItemInfoScript.cs
@@ -2,9 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class ItemInfoScript : MonoBehaviour
 {
     public float prijs;
     public float Duration;
+    public BinType Type;
 
+}
+public enum BinType
+{
+    RED,
+    ORANGE,
+    GREEN
 }
